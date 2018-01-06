d059ea96d83561ef55280410d5fb7830d00f6000
NTvirus <ntvirus333@gmail.com>
2018-01-06 20:47:55 +0545

    Working links added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 # Create your views here.
 
 def post_list(request):
-	context={}
+	posts=Post.objects.order_by("created_date")
+	context={'posts':posts}
 	return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+	post=get_object_or_404(Post,pk=pk)
+	context={"post":post}
+	return render(request, 'blog/post_detail.html', context)
